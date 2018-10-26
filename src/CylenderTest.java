8a0e5a8f95cc89ac90732bbe8f0cf09fa9c9a550
TinyChicken12 <Tnkien12vcu@gmail.com>
2018-10-26 17:12:45 +0700

    add files

---
+++ src/CylenderTest.java
@@ -0,0 +1,7 @@
+public class CylenderTest {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder(4.5,"Green");
+
+        System.out.println(cylinder.toString());
+    }
+}
